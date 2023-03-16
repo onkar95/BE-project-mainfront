88d2dc958b0fc95792d4e084d7a190e6eb7d6a14
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-03-17 00:14:38 +0530

    chore: remove unused import

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Education from './Education'
 import Experience from './Experience'
 import Preferences from './Preferences'
