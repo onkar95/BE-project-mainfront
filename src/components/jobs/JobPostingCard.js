506c6274366dbbb174df7c0ba5fcad6ccf33c647
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-06-01 03:12:25 +0530

    fix: job posting card ui

--- src/components/jobs/JobPostingCard.js
+++ src/components/jobs/JobPostingCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./job.css";
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import { useNavigate } from "react-router-dom";
 
 
 const JobPostingsCard = ({ id, title, date, skills, budget, applicants, shortlisted, interviewed }) => {
+
   const navigate = useNavigate();
   let exactDate = date.slice(0, 10)
 
@@ -17,16 +18,14 @@ const JobPostingsCard = ({ id, title, date, skills, budget, applicants, shortlis
 
   const removeThisItem = (e, id) => {
     e.stopPropagation();
+
   }
 
   return (
     <div className="job-postings-card-container" onClick={navigateToPostingDetail}>
       <div className="job-postings-card-header">
-        <h3>{title} <span style={{ fontSize: "0.9em", fontWeight: "300", color: "#333", marginLeft: "12px" }} >{exactDate}</span></h3>
+        <h3>{title}</h3>
         <div className="job-postings-card-actions-container">
-          <div className="job-postings-card-action-item">
-            <EditOutlinedIcon />
-          </div>
           <div className="job-postings-card-action-item">
             <DeleteOutlineOutlinedIcon onClick={(e) => removeThisItem(e, id)} />
           </div>
@@ -39,12 +38,21 @@ const JobPostingsCard = ({ id, title, date, skills, budget, applicants, shortlis
             <div className="job-postings-stats-text">
               <p className="job-postings-stats-text-heading">Budget</p>
               <p className="job-postings-stats-text-subtitle">
-                {budget}
+                {budget} LPA
               </p>
             </div>
           </div>
           <div className="job-postings-stats-card">
             <CalendarMonthOutlinedIcon style={{ fontSize: 40 }} />
+            <div className="job-postings-stats-text">
+              <p className="job-postings-stats-text-heading">Date</p>
+              <p className="job-postings-stats-text-subtitle">
+                {exactDate}
+              </p>
+            </div>
+          </div>
+          <div className="job-postings-stats-card">
+            <WorkspacePremiumIcon style={{ fontSize: 40 }} />
             <div className="job-postings-stats-text">
               <p className="job-postings-stats-text-heading">Skills</p>
               <p className="job-postings-stats-text-subtitle">
@@ -54,28 +62,8 @@ const JobPostingsCard = ({ id, title, date, skills, budget, applicants, shortlis
               </p>
             </div>
           </div>
-        </div>
-        <div className="divider"></div>
-        <div className="job-postings-metadata-container">
-          <div className="job-postings-metadata-card">
-            <p className="job-postings-metadata-text-heading">{applicants}</p>
-            <p className="job-postings-metadata-text-subtitle">
-              Applicants
-            </p>
-          </div>
-          <div className="job-postings-metadata-card">
-            <p className="job-postings-metadata-text-heading">{shortlisted}</p>
-            <p className="job-postings-metadata-text-subtitle">
-              Shortlisted
-            </p>
-          </div>
-          <div className="job-postings-metadata-card">
-            <p className="job-postings-metadata-text-heading">{interviewed}</p>
-            <p className="job-postings-metadata-text-subtitle">
-              Interviewed
-            </p>
-          </div>
-        </div>
+          
+        </div>        
       </div>
     </div>
   );
