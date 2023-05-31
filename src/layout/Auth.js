d246eafece310b1d7268fe6902982396efee55fe
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-06-01 03:24:53 +0530

    style: remove nav in auth screens

--- src/layout/Auth.js
+++ src/layout/Auth.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Header from '../components/navbar/Header'
 
 const Auth = ({ Component }) => {
     return (
         <div className='App'>
-            <div className='header_sidebar'>
-                <Header />
-            </div>
             <div className='components'>
                 <Component />
             </div>
